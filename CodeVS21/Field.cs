bb222ff36cf7d77b02341cfbfeda9e1f136247ca
misogi <dev@misogi.info>
2013-06-07 17:12:23 +0900

    Add Field Class

---
+++ CodeVS21/Field.cs
@@ -0,0 +1,39 @@
+﻿namespace CodeVS21
+{
+    /// <summary>
+    /// The field.
+    /// </summary>
+    public class Field
+    {
+        #region Fields
+
+        /// <summary>
+        /// The cell.
+        /// </summary>
+        private int[,] cell;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Field"/> class.
+        /// </summary>
+        /// <param name="width">
+        /// The width.
+        /// </param>
+        /// <param name="height">
+        /// The height.
+        /// </param>
+        public Field(int width, int height)
+        {
+            this.cell = new int[width, height];
+        }
+
+        public void Fall(Pack pack, int position, int rotate)
+        {
+            
+        }
+        #endregion
+    }
+}
---
+++ CodeVS21Tests/FieldTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeVS21Tests
+{
+    using CodeVS21;
+    [TestClass]
+    public class FieldTest
+    {
+        [TestMethod]
+        public void NewField()
+        {
+            var f = new Field(3, 2);
+            Assert.IsNotNull(f);
+        }
+    }
+}
